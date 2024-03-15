0c90922b12646210be14e579f388b132271241a4
CT <138801309+CT62@users.noreply.github.com>
2024-03-15 18:03:06 +0000

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -1,1 +1,11 @@
-cat ./i3/config
+sudo mv ./i3/config ~/.config/i3/
+sudo pacman -S install --noconfirm polybar
+sudo mv ./polybar/config.ini ~/.config/polybar/
+curl -sS https://starship.rs/install.sh | sh
+sudo mv ./starship.toml ~/.config/
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
+git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
+sudo pacman -S --noconfirm picom
+sudo pacman -S --noconfirm xorg-xrandr
+sudo pacman -S --noconfirm ttf-jetbrains-mono-nerd
+sudo pacman -S --noconfirm ttf-nerd-fonts-symbols
