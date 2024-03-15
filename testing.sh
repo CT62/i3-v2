09bfc47dafdaf08718243bb755d5130b5060ff99
CT <138801309+CT62@users.noreply.github.com>
2024-03-15 19:01:53 +0000

    Update testing.sh

--- testing.sh
+++ testing.sh
@@ -3,6 +3,7 @@ sudo pacman -S install --noconfirm polybar
 sudo mv ./polybar/config.ini ~/.config/polybar/
 curl -sS https://starship.rs/install.sh | sh
 sudo mv ./starship.toml ~/.config/
+sudo mv ./alacritty.yml ~/.config/alacritty/
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 git clone https://github.com/NvChad/starter ~/.config/nvim && nvim
 sudo pacman -S --noconfirm picom
